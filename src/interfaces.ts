7657863edbcc02ef3dcab881fa1f7d9ac3723405
wojbiel833 <wojbiel833@gmail.com>
2022-04-12 08:48:20 +0200

    Changed some syntax III

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,3 +8,7 @@ export interface Country {
   population: number;
   regionalBlocs?: RegionalBlocs[];
 }
+
+export function SetKey<A extends Country>(object: A, key: keyof A): A[keyof A] {
+  return object[key];
+}
